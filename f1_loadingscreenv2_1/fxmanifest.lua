6468a3211e2d1a31deaf7112f612c7a5900881db
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2022-03-21 20:41:06 +0000

    LOADING-SCREEN V2.1
    AUTOPLAY - ADD
    VIDEO - ADD
    IMAGE - REMOVED

---
+++ f1_loadingscreenv2_1/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "F1"
+description "discord.gg/Nb23tC3wmA"
+version "2.0"
+
+files {
+    'index.html',
+    'audio/*.mp3',
+    'css/styles.css',
+    'video/*',
+    'img/*',
+    'img/*/*',
+    'js/main.js',
+}
+
+loadscreen 'index.html'
